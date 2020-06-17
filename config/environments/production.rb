b642d5e0ca19aa1961995cc9645dc07b42e7a0b1
Rashmi Agarwal <rashmiagar@gmail.com>
2020-06-17 20:32:18 +0530

    Changed smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
   config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
+  config.action_mailer.perform_deliveries = true
 
   # Configure your SMTP service credentials in secrets.yml
   if Rails.application.secrets.smtp_settings
