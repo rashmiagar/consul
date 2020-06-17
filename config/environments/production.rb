748dae757ddcbbafde727d7c51cd7f307ecc062a
Rashmi Agarwal <rashmiagar@gmail.com>
2020-06-17 20:54:38 +0530

    Adding secret

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
   config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"  
+  
 
   # Configure your SMTP service credentials in secrets.yml
   if Rails.application.secrets.smtp_settings
