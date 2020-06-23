854dbc1df4fa71c6c7cf8d64c1e8116126e84543
Rashmi Agarwal <rashmiagar@gmail.com>
2020-06-24 00:29:43 +0530

    Added rack timeout

---
+++ config/initializers/rack_timeout.rb
@@ -0,0 +1,7 @@
+# Rack::Timeout.service_timeout = Rails.env.development? ? 0 : 25
+if Rails.env.development?
+  puts "in dev"
+  Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 0
+else 
+  Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 0
+end
