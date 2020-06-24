d557ef2fbd5c12f46bdff38d0f49c92f7f607d5f
Rashmi Agarwal <rashmiagar@gmail.com>
2020-06-24 13:50:44 +0530

    added social oauth

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -3,5 +3,5 @@ if Rails.env.development?
   puts "in dev"
   Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 0
 else 
-  Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 0
+  Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 25
 end
